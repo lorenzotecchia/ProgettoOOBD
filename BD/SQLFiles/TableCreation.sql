c8c053562f9e20096a08854887d167c9df18bc11
Lorenzo Tecchia <rueful.chest_0f@icloud.com>
2023-01-06 15:59:16 +0100

    eliminato le doppie virgoletteper un problema di caps sensitivity

--- BD/SQLFiles/TableCreation.sql
+++ BD/SQLFiles/TableCreation.sql
@@ -1,4 +1,4 @@
--- drop schema mtl cascade;
+drop schema mtl cascade;
 create schema mtl;
 
 create type mtl.AccessMode as enum (
@@ -9,12 +9,12 @@ create type mtl.AccessMode as enum (
 
 create table mtl.Event
 (
-    "CodEvent"         serial
+    CodEvent         serial
         primary key,
-    "StartDate"        timestamp,
-    "EndDate"          timestamp,
-    "LocationPromoter" varchar(20),
-    "Manager"          varchar(20)
+    StartDate        timestamp,
+    EndDate          timestamp,
+    LocationPromoter varchar(20),
+    Manager          varchar(20)
 
 );
 
@@ -22,74 +22,58 @@ comment on table mtl.event is 'Creation of the table event';
 
 create table mtl.Book
 (
-    "Doi_B"           varchar(50)
+    Doi_B           varchar(50)
         primary key,
     ISBN_B            varchar(20)
         unique,
-    "Edtion"          int,
-    "ReleaseDate"     timestamp,
-    "PublishingHouse" varchar(20),
-    "Author"          varchar(60),
-    "AccessMode"      mtl.AccessMode,
-    "Title"           varchar(10),
-    "Argument"        varchar(10),
-    "Reprint"          boolean,
-    "FK_Series"       varchar(20)
+    Edtion          int,
+    ReleaseDate     timestamp,
+    PublishingHouse varchar(20),
+    Author          varchar(60),
+    AccessMode      mtl.AccessMode,
+    Title           varchar(10),
+    Argument        varchar(10),
+    Reprint          boolean,
+    FK_Series       varchar(20)
 );
 
 create table mtl.Series
 (
     ISSN_S       varchar(20) primary key,
-    "Curator"    varchar(20),
-    "Edition"    int,
-    "Name"       varchar(10),
-    "AccessMode" mtl.accessmode
+    Curator    varchar(20),
+    Edition    int,
+    NameS      varchar(10),
+    AccessMode mtl.accessmode
 );
 create table mtl.Magazine
 (
     ISSN_M        varchar(20) primary key,
-    "Argument"    varchar(10),
-    "Manager"     varchar(20),
-    "YearRelease" timestamp,
-    "NameM"       varchar(10),
-    "AccessMode"  mtl.accessmode
+    Argument    varchar(10),
+    Manager     varchar(20),
+    YearRelease timestamp,
+    NameM       varchar(10),
+    AccessMode  mtl.accessmode
 );
 
 comment on table mtl.Magazine is 'Creation of the table Magazine';
 
 create table mtl.Article
 (
-    "Doi_A"       varchar(20)
+    Doi_A       varchar(20)
         primary key,
-    "Title"       varchar(40),
-    "AccessMode"  mtl.accessmode,
-    "YearRelease" timestamp,
-    "Editor"      varchar(10),
-    "Author"      varchar(60),
-    "FK_Magazine" varchar(20)
+    Title       varchar(40),
+    AccessMode  mtl.accessmode,
+    YearRelease timestamp,
+    Editor      varchar(10),
+    Author      varchar(60),
+    FK_Magazine varchar(20)
 );
 
 comment on table mtl.Article is 'Creation of table Article';
 
-create table mtl.Loan
-(
-    "LoanCode"  serial
-        primary key,
-    "StartLoan" timestamp,
-    "EndLoan"   timestamp,
-    "FK_User"   varchar(11),
-
-    constraint LoanFK foreign key ("FK_User") references mtl.User ("SSN")
-);
-
-alter table mtl.Loan
-    owner to "lorenzotecchia";
-
-comment on table mtl.Loan is 'Creation of the table Loan';
-
 create table mtl.User
 (
-    "SSN" varchar(20)
+    SSN varchar(20)
         primary key,
     password varchar(100)
         unique
@@ -97,13 +81,27 @@ create table mtl.User
 
 comment on table mtl.User is 'Creation of the table User';
 
+create table mtl.Loan
+(
+    LoanCode  serial
+        primary key,
+    StartLoan timestamp,
+    EndLoan   timestamp,
+    FK_User   varchar(11),
+
+    constraint LoanFK foreign key (FK_User) references mtl.User (SSN)
+);
+
+
+comment on table mtl.Loan is 'Creation of the table Loan';
+
 create table mtl.Discussion
 (
     FK_Event   serial,
     FK_Article varchar(20),
 
-    constraint DiscussionFK_1 foreign key (FK_Event) references mtl.Event ("CodEvent"),
-    constraint DiscussionFK_2 foreign key (FK_Article) references mtl.article ("Doi_A")
+    constraint DiscussionFK_1 foreign key (FK_Event) references mtl.Event (CodEvent),
+    constraint DiscussionFK_2 foreign key (FK_Article) references mtl.article (Doi_A)
 );
 
 comment on table mtl.Discussion is 'Creation of the table';
@@ -114,8 +112,8 @@ create table mtl.Presentation
     FK_Event serial,
     FK_Book  varchar(20),
 
-    constraint PresentationFK_1 foreign key (FK_Event) references mtl.Event ("CodEvent"),
-    constraint PresentationFK_2 foreign key (FK_Book) references mtl.Book ("Doi_B")
+    constraint PresentationFK_1 foreign key (FK_Event) references mtl.Event (CodEvent),
+    constraint PresentationFK_2 foreign key (FK_Book) references mtl.Book (Doi_B)
 );
 
 comment on table mtl.Presentation is 'Creation of the table Presentation';
@@ -128,11 +126,11 @@ create table mtl.Drawing
     Fk_Magazine varchar(20),
     Fk_Book     varchar(20),
 
-    constraint DrawingFK_1 foreign key (FK_LoanCode) references mtl.Loan ("LoanCode"),
-    constraint DrawingFK_2 foreign key (FK_Article) references mtl.Article ("Doi_A"),
-    constraint DrawingFK_3 foreign key (Fk_Book) references mtl.Book ("Doi_B"),
-    constraint DrawingFK_4 foreign key (Fk_Series) references mtl.Series ("issn_s"),
-    constraint DrawingFK_5 foreign key (Fk_Magazine) references mtl.Magazine ("issn_m")
+    constraint DrawingFK_1 foreign key (FK_LoanCode) references mtl.Loan (LoanCode),
+    constraint DrawingFK_2 foreign key (FK_Article) references mtl.Article (Doi_A),
+    constraint DrawingFK_3 foreign key (Fk_Book) references mtl.Book (Doi_B),
+    constraint DrawingFK_4 foreign key (Fk_Series) references mtl.Series (issn_s),
+    constraint DrawingFK_5 foreign key (Fk_Magazine) references mtl.Magazine (issn_m)
 );
 
 comment on table mtl.Drawing is 'Creation of the table Drawing';
--- BD/SQLFiles/views.sql
+++ BD/SQLFiles/views.sql
@@ -1,8 +1,8 @@
 --Tabella virtuale con tutti i libri disponibili
 create view mtl.available_books as
-    select distinct b."Doi_B"
+    select distinct b.Doi_B
     from mtl.book b
-    where b."AccessMode" = 'Paper'
+    where b.AccessMode = 'Paper'
     except
         (select distinct b.fk_book
         from mtl.drawing b);
@@ -10,9 +10,9 @@ create view mtl.available_books as
 
 --Tabella virtuale con tutti gli articoli disponibili
 create view mtl.available_articles as
-    select distinct a."Doi_A"
+    select distinct a.Doi_A
     from mtl.article a
-    where a."AccessMode" = 'Paper'
+    where a.AccessMode = 'Paper'
     except
         (select distinct b.fk_article
         from mtl.drawing b);
@@ -22,7 +22,7 @@ create view mtl.available_articles as
 create view mtl.available_series as
     select distinct s.issn_s
     from mtl.series s
-    where s."AccessMode" = 'Paper'
+    where s.AccessMode = 'Paper'
     except
         (select distinct b.fk_series
         from mtl.drawing b);
@@ -32,8 +32,12 @@ create view mtl.available_series as
 create view mtl.available_magazine as
     select distinct m.issn_m
     from mtl.magazine m
-    where m."AccessMode" = 'Paper'
+    where m.AccessMode = 'Paper'
     except
         (select distinct b.fk_magazine
         from mtl.drawing b);
 
+
+
+select distinct b.Title
+from mtl.available_books av natural join mtl.book b;
