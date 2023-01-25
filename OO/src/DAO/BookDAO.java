02c2dc4dc0c10a1ecd004aec9ed6b2980ba3c8e8
Lgegmb <118052439+Lgegmb@users.noreply.github.com>
2023-01-25 15:02:49 +0100

    book e bookdao correction

--- OO/src/DAO/BookDAO.java
+++ OO/src/DAO/BookDAO.java
@@ -17,20 +17,23 @@ public class BookDAO {
         this.connection = connection;
     }
 
-    public void create(String doi_B, String ISBN_B, int edition, Timestamp releaseDate, String publishingHouse, String author, String accessMode, String title, String argument, boolean reprint, String FK_Series) throws SQLException {
-        String query = "INSERT INTO mtl.Book (Doi_B, ISBN_B, Edition, ReleaseDate, PublishingHouse, Author, AccessMode, Title, Argument, Reprint, FK_Series) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    public void create(String doi_B, String ISBN_B, int edition, String publishingHouse, String language, String title, String accessMode, String argument, boolean reprint, Timestamp releaseDate, String releaseLocation, String PresentationName, String FK_author, String FK_Series) throws SQLException {
+        String query = "INSERT INTO mtl.Book (Doi_B, ISBN_B, Edition, PublishingHouse, Language, Title, Argument, AccessMode, Reprint, ReleaseDate, ReleaseLocation, PresentationName, FK_author, FK_Series) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, doi_B);
             statement.setString(2, ISBN_B);
             statement.setInt(3, edition);
-            statement.setTimestamp(4, releaseDate);
-            statement.setString(5, publishingHouse);
-            statement.setString(6, author);
-            statement.setString(7, accessMode);
-            statement.setString(8, title);
-            statement.setString(9, argument);
-            statement.setBoolean(10, reprint);
-            statement.setString(11, FK_Series);
+            statement.setString(4 , publishingHouse);
+            statement.setString(5, language);
+            statement.setString(6, title);
+            statement.setString(7, argument);
+            statement.setString(8, accessMode);
+            statement.setBoolean(9, reprint);
+            statement.setTimestamp(10, releaseDate);
+            statement.setString(11, releaseLocation);
+            statement.setString(12, PresentationName);
+            statement.setString(13, FK_author);
+            statement.setString(14, FK_Series);
             statement.executeUpdate();
         }
     }
@@ -44,15 +47,18 @@ public class BookDAO {
                 String doi_B = resultSet.getString("Doi_B");
                 String ISBN_B = resultSet.getString("ISBN_B");
                 int edition = resultSet.getInt("Edition");
-                Timestamp releaseDate = resultSet.getTimestamp("ReleaseDate");
                 String publishingHouse = resultSet.getString("PublishingHouse");
-                String author = resultSet.getString("Author");
-                String accessMode = resultSet.getString("AccessMode");
+                String language = resultSet.getString("Language");
                 String title = resultSet.getString("Title");
                 String argument = resultSet.getString("Argument");
+                String accessMode = resultSet.getString("AccessMode");
                 boolean reprint = resultSet.getBoolean("Reprint");
+                Timestamp releaseDate = resultSet.getTimestamp("ReleaseDate");
+                String releaseLocation = resultSet.getString("ReleaseLocation");
+                String presentationName = resultSet.getString("PresentationName");
+                String Fk_author = resultSet.getString("FK_author");
                 String FK_Series = resultSet.getString("FK_Series");
-                books.add(new Book(doi_B, ISBN_B, edition, releaseDate, publishingHouse, author, accessMode, title, argument, reprint, FK_Series));
+                books.add(new Book(doi_B, ISBN_B, edition, publishingHouse,language, title, argument, accessMode, reprint, releaseDate, releaseLocation, presentationName,Fk_author, FK_Series));
             }
         }
         return books;
--- OO/src/Model/Book.java
+++ OO/src/Model/Book.java
@@ -4,24 +4,40 @@ import java.sql.Timestamp;
 
 public class Book {
 
-    private final String ISNb_b;
+    private String ISNb_b;
     private String doi_B;
+    private int edition;
+    private String publishingHouse;
+    private String language;
     private String title;
     private String accessMode;
-    private Timestamp yearRelease;
-    private int edition;
-    private String author;
-    private String FK_Magazine;
+    private String argument;
+    private Boolean reprint;
+    private Timestamp releaseDate;
+    private String releaseLocation;
+    private String presentationName;
+    private String FK_author;
+    private String FK_Series;
 
-    public Book(String doi_B, String ISNb_b,String title, String accessMode, Timestamp yearRelease,int edition, String author, String FK_Magazine) {
+
+    public Book(String doi_B, String ISNb_b, int edition, String publishingHouse, String language, String title, String accessMode, String argument, Boolean reprint, Timestamp releaseDate, String releaseLocation, String presentationName, String FK_author, String FK_Series) {
         this.ISNb_b = ISNb_b;
         this.doi_B = doi_B;
+        this.edition = edition;
+        this.publishingHouse = publishingHouse;
+        this.language = language;
         this.title = title;
         this.accessMode = accessMode;
-        this.yearRelease = yearRelease;
-        this.edition = edition;
-        this.author = author;
-        this.FK_Magazine = FK_Magazine;
+        this.argument = argument;
+        this.reprint = reprint;
+        this.releaseDate = releaseDate;
+        this.releaseLocation = releaseLocation;
+        this.presentationName = presentationName;
+        this.FK_author = FK_author;
+        this.FK_Series = FK_Series;
+    }
+
+    public Book(String doiB, String isbnB, int edition, String publishingHouse, String language, String title, String argument, String accessMode, boolean reprint, Timestamp releaseDate, String releaseLocation, String presentationName, String fkAuthor, String fkSeries) {
     }
 
     public String getDoi_B() {
@@ -48,13 +64,11 @@ public class Book {
         this.accessMode = accessMode;
     }
 
-    public Timestamp getYearRelease() {
-        return yearRelease;
+    public Timestamp getReleaseDate() {
+        return releaseDate;
     }
 
-    public void setYearRelease(Timestamp yearRelease) {
-        this.yearRelease = yearRelease;
-    }
+    public void setReleaseDate(Timestamp releaseDate) { this.releaseDate = releaseDate; }
 
     public int getEdition() {
         return edition;
@@ -64,32 +78,57 @@ public class Book {
         this.edition = edition;
     }
 
-    public String getAuthor() {
-        return author;
-    }
+    public String getAuthor() { return FK_author; }
 
     public void setAuthor(String author) {
-        this.author = author;
+        this.FK_author = author;
     }
 
-    public String getFK_Magazine() {
-        return FK_Magazine;
+    public String getFK_Series() {
+        return FK_Series;
     }
 
-    public void setFK_Magazine(String FK_Magazine) {
-        this.FK_Magazine = FK_Magazine;
+    public void setFK_Series(String FK_Series) {
+        this.FK_Series = FK_Series;
     }
+    public Boolean getReprint() {return reprint; }
+
+    public void setReprint(Boolean reprint) { this.reprint = reprint; }
+
+    public String getArgument() { return argument; }
+
+    public void setArgument(String argument) { this.argument = argument; }
+
+    public String getLanguage() { return language; }
+
+    public void setLanguage(String language) { this.language = language; }
+
+    public String getISNb_b() { return ISNb_b; }
+    public void setISNb_b(String ISNb_b) { this.ISNb_b = ISNb_b; }
+    public String getPublishingHouse() { return publishingHouse; }
+    public void setPublishingHouse(String publishingHouse) { this.publishingHouse = publishingHouse; }
+    public String getReleaseLocation() { return releaseLocation; }
+    public void setReleaseLocation(String releaseLocation) { this.releaseLocation = releaseLocation; }
+    public String getPresentationName() { return presentationName; }
+    public void setPresentationName(String presentationName) { this.presentationName = presentationName; }
 
     @Override
     public String toString() {
         return "Book{" +
                 "doi_B='" + doi_B + '\'' +
+                ", ISNb_b='" + ISNb_b + '\'' +
+                ", edition='" + edition + '\'' +
+                ", publishingHouse='" + publishingHouse + '\'' +
+                ", language='" + language + '\'' +
                 ", title='" + title + '\'' +
+                ", argument='" + argument + '\'' +
                 ", accessMode='" + accessMode + '\'' +
-                ", yearRelease=" + yearRelease +
-                ", editor='" + edition + '\'' +
-                ", author='" + author + '\'' +
-                ", FK_Magazine='" + FK_Magazine + '\'' +
+                ", reprint='" + reprint + '\'' +
+                ", releaseDate=" + releaseDate + '\'' +
+                ", releaseLocation='" + releaseLocation + '\'' +
+                ", presentationName='" + presentationName + '\'' +
+                ", FK_Series='" + FK_Series + '\'' +
+                ", FK_author='" + FK_author + '\'' +
                 '}';
     }
 }
